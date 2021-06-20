fce16cc1d794d100535cb7e5a523a26949fb0308
Janainaf <figueira.janaina@gmail.com>
2021-06-20 08:55:08 +0200

    created edit form on page, need to add modal

---
+++ src/pages/EditVenue/index.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { useHistory, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Cookies from "js-cookie";
+import config from "config";
+import "pages/Venue/Venue.css";
+import Booking from "pages/Booking";
+
+const EditVenue = ({ venues }) => {
+  const { idVenue } = useParams();
+  const [currentVenue, setCurrentVenue] = useState(null);
+  const token = Cookies.get(config.COOKIE_STORAGE_KEY);
+  const [name, setName] = useState();
+  const [city, setCity] = useState();
+  const [cuisine, setCuisine] = useState();
+  const history = useHistory();
+  const currentUser = useSelector((state) => state.authReducer);
+  const [seat, setSeat] = useState();
+  const [time, setTime] = useState();
+  const [date, setDate] = useState();
+  const userId = useSelector((state) => state.authReducer.id);
+
+  const dataVenue = {
+    venue: {
+      name: name,
+      city: city,
+      cuisine: cuisine,
+    },
+  };
+
+  console.log(currentUser);
+  const fetchEditVenue = async (e) => {
+    e.preventDefault();
+    const response = await fetch(
+      `https://trouvetatableapi.herokuapp.com/api/venues/${idVenue}`,
+      {
+        method: "put",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataVenue),
+      }
+    );
+
+    if (response) {
+      history.push("/");
+      return;
+    }
+
+    const data = await response.json();
+    console.log("data", data);
+  };
+
+
+  useEffect(() => {
+    fetch(`https://trouvetatableapi.herokuapp.com/api/venues/${idVenue}`)
+      .then((response) => response.json())
+      .then((data) => setCurrentVenue(data));
+  }, [idVenue]);
+
+  return (
+    <div className="container-page d-flex align-items-center justify-content-center  ">
+      <div className="form-container">
+        <h3> Edit Venue</h3>
+        <form>
+          <div>
+            <label type="text" name="venuename">
+              Name
+          </label>
+            <input
+              type="text"
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <label type="text" name="city">
+              City
+          </label>
+            <input
+              type="text"
+              name="city"
+              onChange={(e) => setCity(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <label type="text" name="cuisine">
+              Cuisine
+          </label>
+            <input
+              type="text"
+              name="cuisine"
+              onChange={(e) => setCuisine(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <button type="submit" onClick={fetchEditVenue}>
+              Edit Venue
+          </button>
+          </div>
+        </form>
+      </div>
+    </div>
+    
+  );
+};
+
+export default EditVenue;
