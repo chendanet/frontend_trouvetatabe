d8c712d95d9168da07cd7ddf98b170932668a529
YoucefKhalainou <youcef_khalainou@yahoo.fr>
2021-06-22 15:39:08 +0200

    filter by cuisine category

--- src/pages/ListVenue/index.jsx
+++ src/pages/ListVenue/index.jsx
@@ -2,39 +2,53 @@ import React from "react";
 import { useEffect, useState } from "react";
 import "pages/ListVenue/listVenue.css";
 import { Link } from "react-router-dom";
+
+
+
+
 const ListVenues = ({ venues }) => {
   const [searchTerme, setSearchTerme] = useState("");
+  const [cuisines, setCuisines] = useState([])
 
-  console.log()
+  const CUISINES = [
+    "Steak",
+    "Seafood",
+    "French",
+    "Asian",
+    "Indian",
+    "Italian",
+    "American",
+    "Mexican",
+    "Californian",
+    "Korean"
+  ]
 
+  const handleChangeCuisine = (e) => {
+    let { name } = e.target
+
+    if (cuisines.indexOf(name) == -1) {
+      setCuisines([...cuisines, name])
+    } else {
+      setCuisines(cuisines.filter((p) => p !== name))
+    }
+
+  }
+
+  console.log('cuisines', cuisines.length)
   return (
     <div className="container-list">
       <div className="filter">
-        <ul>
-          <li>categories</li>
-          <li>categories</li>
-          <li>categories</li>
-        </ul>
-        <ul>
-          <li>categories</li>
-          <li>categories</li>
-          <li>categories</li>
-        </ul>
-        <ul>
-          <li>categories</li>
-          <li>categories</li>
-          <li>categories</li>
-        </ul>
-        <ul>
-          <li>categories</li>
-          <li>categories</li>
-          <li>categories</li>
-        </ul>
-        <ul>
-          <li>categories</li>
-          <li>categories</li>
-          <li>categories</li>
-        </ul>
+        {CUISINES.map((c, index) => (
+          <div key={index}>
+            <input type="checkbox"
+              name={c}
+              value={c}
+              onChange={handleChangeCuisine}
+              checked={cuisines.includes(c) ? "checked" : ""}
+            />
+            <label>{c}</label>
+          </div>
+        ))}
       </div>
       <div className="container-img-item">
         <form>
@@ -49,12 +63,24 @@ const ListVenues = ({ venues }) => {
         {venues
           .filter((value) => {
             if (searchTerme == "") {
-              return value;
-            } else if (
-              value.name.toLowerCase().includes(searchTerme.toLowerCase())
-            ) {
+              if (cuisines.length == 0) {
+                return value;
+              }
+            }
+            if (value.name.toLowerCase().includes(searchTerme.toLowerCase()) && cuisines.length === 0) {
               return value;
             }
+            if (searchTerme == "") {
+              if (cuisines.indexOf(value.cuisine) >= 0) {
+                return value;
+              }
+            }
+            if (value.name.toLowerCase().includes(searchTerme.toLowerCase())) {
+              if (cuisines.indexOf(value.cuisine) >= 0) {
+                return value;
+              }
+            }
+
           })
           .map((item, index) => (
             <div className="image-item" key={index}>
