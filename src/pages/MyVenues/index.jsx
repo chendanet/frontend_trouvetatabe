fc3369d26fbb78b96cd00706d69655066c3282c9
YoucefKhalainou <youcef_khalainou@yahoo.fr>
2021-06-23 18:19:50 +0200

    fix ternary in src image

--- src/pages/MyVenues/index.jsx
+++ src/pages/MyVenues/index.jsx
@@ -16,9 +16,18 @@ const MyVenues = ({ venues }) => {
             {venues
                 .filter((value) => value.user_id == currentManager.id)
                 .map((item, index) => (
-                    <div className="image-item row w-100 " key={index}>
-                       
-                        <img src={`https://source.unsplash.com/600x600/?dish&sig=${index}}`} alt="" className="col-md-5" />
+                    <div className="image-item row w-25 " key={index}>
+                        {!item.images[0] ?
+                            <img
+                                src={`https://source.unsplash.com/600x600/?dish&sig=${index}`}
+                                alt={`${item.name}_image`}
+                                className="img-fluid card-border"
+                            />
+                            : <img
+                                src={item.images[0]}
+                                alt={`${item.name}_image`}
+                                className="img-fluid card-border"
+                            />}
                         <Link to={"/venues/" + item.id} className="col-md-5">
                             <div className="container-item">
                                 <h5>{item.name}</h5>
