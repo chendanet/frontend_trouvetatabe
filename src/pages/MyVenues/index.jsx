fd72a142935625190826e62c3b7ecc60e952cb61
Charles <charlescampet03@gmail.com>
2021-06-23 14:25:06 +0200

    remove some empty lines + try ternary for display images

--- src/pages/MyVenues/index.jsx
+++ src/pages/MyVenues/index.jsx
@@ -2,9 +2,6 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 
-
-
-
 const MyVenues = ({ venues }) => {
 
     const currentManager = useSelector((state) => state.authReducer)
@@ -15,7 +12,7 @@ const MyVenues = ({ venues }) => {
                 .filter((value) => value.user_id === currentManager.id)
                 .map((item, index) => (
                     <div className="image-item" key={index}>
-                        <img src={`https://source.unsplash.com/600x600/?dish&sig=${index}}`} alt="" />
+                        <img src={venues.photo.empty ? venues.image : `https://source.unsplash.com/600x600/?dish&sig=${index}}`} alt="" />
                         <Link to={"/venues/" + item.id}>
                             <div className="container-item">
                                 <h5>{item.name}</h5>
@@ -28,7 +25,5 @@ const MyVenues = ({ venues }) => {
         </div>
     )
 }
-
-
 
 export default MyVenues
