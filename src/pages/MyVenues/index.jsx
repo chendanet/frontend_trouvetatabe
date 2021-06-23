d4c052dad6f10f40bd4e5c7c7fa4c20a6952f5a5
Charles <charlescampet03@gmail.com>
2021-06-23 14:46:29 +0200

    adding ternary for images

--- src/pages/MyVenues/index.jsx
+++ src/pages/MyVenues/index.jsx
@@ -12,16 +12,9 @@ const MyVenues = ({ venues }) => {
             {venues
                 .filter((value) => value.user_id === currentManager.id)
                 .map((item, index) => (
-<<<<<<< HEAD
                     <div className="image-item" key={index}>
                         <img src={venues.photo.empty ? venues.image : `https://source.unsplash.com/600x600/?dish&sig=${index}}`} alt="" />
                         <Link to={"/venues/" + item.id}>
-=======
-                    <div className="image-item row w-100 " key={index}>
-                       
-                        <img src={`https://source.unsplash.com/600x600/?dish&sig=${index}}`} alt="" className="col-md-5" />
-                        <Link to={"/venues/" + item.id} className="col-md-5">
->>>>>>> develop
                             <div className="container-item">
                                 <h5>{item.name}</h5>
                                 <p>{item.city}</p>
