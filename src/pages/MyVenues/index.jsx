c355232382121f0b4fc750389c927da566c03885
chendanet <chenda_net@yahoo.fr>
2021-06-26 10:28:09 +0200

    add bootstrap to card

--- src/pages/MyVenues/index.jsx
+++ src/pages/MyVenues/index.jsx
@@ -2,6 +2,7 @@ import { useSelector} from "react-redux";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { PROD_EDIT_VENUE } from 'api/apiHandler';
+import Card from 'react-bootstrap/Card';
 
 
 
@@ -21,37 +22,43 @@ useEffect(() => {
     const currentManager = useSelector((state) => state.authReducer)
     console.log('venues', venues)
     console.log('venues', currentManager)
+
     return (
-        <div className="container text-center ">
-            <h2 className="m-5">Mes restaurants:</h2>
-            {venues && venues
-                .filter((value) => value.user_id === parseInt(currentManager.id))
-                .map((item, index) => (
+        <Card style={{ width: '180rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+                <Card.Title>Your venue(s)</Card.Title>
+                <Card.Text>
+                    Here, your information the customers can see :
+                </Card.Text>
+                {venues && venues
+                    .filter((value) => value.user_id === parseInt(currentManager.id))
+                    .map((item, index) => (
 
-                    <div className="image-item row w-25 " key={index}>
-                        {!item.images[0] ?
-                            <img
-                                src={`https://source.unsplash.com/600x600/?dish&sig=${index}`}
-                                alt={`${item.name}_image`}
-                                className="img-fluid card-border"
-                            />
-                            : <img
-                                src={item.images[0]}
-                                alt={`${item.name}_image`}
-                                className="img-fluid card-border"
-                            />}
+                        
+                            {!item.images[0] ?
+                                <img
+                                    src={`https://source.unsplash.com/600x600/?dish&sig=${index}`}
+                                    alt={`${item.name}_image`}
+                                    className="img-fluid card-border"
+                                />
+                                : <img
+                                    src={item.images[0]}
+                                    alt={`${item.name}_image`}
+                                    className="img-fluid card-border"
+                                />}
 
-                        <Link to={"/venues/" + item.id} className="col-md-5">
-                            <div className="container-item">
-                                <h5>{item.name}</h5>
-                                <p>{item.city}</p>
-                                <p>{item.cuisine}</p>
-                            </div>
-                        </Link>
-                    </div>
-                ))}
-        </div>
+                            <Link to={"/venues/" + item.id} className="col-md-5">
+                               
+                                    <h5>{item.name}</h5>
+                                    <p>{item.city}</p>
+                                    <p>{item.cuisine}</p>
+                                
+                            </Link>
+                    ))}
+            </Card.Body>
+        </Card> 
     )
 }
 
-export default MyVenues
+export default MyVenues;
